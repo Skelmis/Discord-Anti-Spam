4d8e30a503a9a5ef748c56207db04e33b8b939e7
skelmis <skelmis.craft@gmail.com>
2020-10-13 23:07:38 +1300

    Add user tests

---
+++ testing/TestUtil/util.py
@@ -0,0 +1,14 @@
+OPTIONS = {
+    "warnThreshold": 3,
+    "kickThreshold": 2,
+    "banThreshold": 2,
+    "messageInterval": 2500,
+    "warnMessage": "Hey $MENTIONUSER, please stop spamming/sending duplicate messages.",
+    "kickMessage": "$USERNAME was kicked for spamming/sending duplicate messages.",
+    "banMessage": "$USERNAME was banned for spamming/sending duplicate messages.",
+    "messageDuplicateCount": 5,
+    "messageDuplicateAccuracy": 90,
+    "ignorePerms": [8],
+    "ignoreUsers": [],
+    "ignoreBots": True,
+}
--- testing/test_user.py
+++ testing/test_user.py
@@ -1,10 +1,12 @@
 import sys
 import unittest
 
-from AntiSpam.Exceptions import DuplicateObject
 
-sys.path.insert(0, "..")
 from AntiSpam import User, Message
+from AntiSpam.Exceptions import DuplicateObject, ObjectMismatch
+
+# Util stuff with regard to options, etc
+from TestUtil import util
 
 
 class TestUser(unittest.TestCase):
@@ -16,9 +18,14 @@ class TestUser(unittest.TestCase):
         """
         Simply setup our User obj before usage
         """
-        self.user = User(0, 3, {})
+        self.user = User(None, 0, 3, util.OPTIONS)
         self.user.messages = Message(0, "Hello world", 0, 2, 3)
         self.user.messages = Message(1, "Foo Bar", 0, 2, 3)
+
+    def test_botAssignment(self):
+        # Given we don't want to populate an entire bot,
+        # lets just check its assigned correctly
+        self.assertIsNone(self.user.bot)
 
     def test_intAssignment(self):
         self.assertIsInstance(self.user.id, int)
@@ -27,9 +34,13 @@ class TestUser(unittest.TestCase):
     def test_listAssignment(self):
         self.assertIsInstance(self.user.messages, list)
 
+    def test_dictAssignment(self):
+        self.assertIsInstance(self.user.options, dict)
+
     def test_valueAssignment(self):
         self.assertEqual(self.user.id, 0)
         self.assertEqual(self.user.guildId, 3)
+        self.assertEqual(self.user.options, util.OPTIONS)
 
         self.user.id = 10
         self.user.guildId = 10
@@ -49,6 +60,32 @@ class TestUser(unittest.TestCase):
     def test_messageRaisesDuplicate(self):
         with self.assertRaises(DuplicateObject):
             self.user.messages = Message(1, "Testing", 0, 2, 3)
+
+    def test_messageRaisesMismatch(self):
+        with self.assertRaises(ObjectMismatch):
+            self.user.messages = Message(20, "Testing", 20, 20, 20)
+
+    def test_repr(self):
+        self.assertEqual(
+            repr(self.user),
+            (
+                f"{self.user.__class__.__name__} object. User id: {self.user.id}, Guild id: {self.user.guildId}, "
+                f"Len Stored Message {len(self.user.messages)}"
+            ),
+        )
+
+    def test_str(self):
+        self.assertEqual(
+            str(self.user), f"{self.user.__class__.__name__} object for {self.user.id}."
+        )
+
+    def test_eqEqual(self):
+        obj = User(None, 0, 3, util.OPTIONS)
+        self.assertTrue(self.user == obj)
+
+    def test_eqNotEqual(self):
+        obj = User(None, 2, 2, util.OPTIONS)
+        self.assertFalse(self.user == obj)
 
 
 if __name__ == "__main__":
