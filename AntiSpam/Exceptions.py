5cd073f12365538c256370cf09fe487b903870ce
skelmis <skelmis.craft@gmail.com>
2020-10-29 21:32:38 +1300

    :white_check_mark: Expand Exception Suite

--- AntiSpam/Exceptions.py
+++ AntiSpam/Exceptions.py
@@ -3,40 +3,26 @@ A file devoted to some nice custom exceptions of mine
 """
 
 
-class DuplicateObject(Exception):
+class BaseASHException(Exception):
+    """A base exception handler for the ASH ecosystem."""
+
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = self.__doc__
+
+    def __str__(self):
+        return self.message
+
+
+class DuplicateObject(BaseASHException):
     """Raised because you attempted to create an object, using the exact same id's as a pre-existing one."""
 
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = self.__doc__
 
-    def __str__(self):
-        return self.message
-
-
-class ObjectMismatch(Exception):
+class ObjectMismatch(BaseASHException):
     """Raised because you attempted add a message to a user, but that user didn't create that message."""
 
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = self.__doc__
 
-    def __str__(self):
-        return self.message
-
-
-class LogicError(Exception):
+class LogicError(BaseASHException):
     """Raised because internal logic has failed. Please create an issue in the github."""
-
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = self.__doc__
-
-    def __str__(self):
-        return self.message
---
+++ testing/test_exceptions.py
@@ -0,0 +1,38 @@
+import sys
+import unittest
+
+
+from AntiSpam.Exceptions import *
+
+
+class TestUser(unittest.TestCase):
+    """
+    Used to test the user object
+    """
+
+    def test_baseException(self):
+        self.assertEqual(
+            "A base exception handler for the ASH ecosystem.", str(BaseASHException())
+        )
+
+    def test_DuplicateObject(self):
+        self.assertEqual(
+            "Raised because you attempted to create an object, using the exact same id's as a pre-existing one.",
+            str(DuplicateObject()),
+        )
+
+    def test_ObjectMismatch(self):
+        self.assertEqual(
+            "Raised because you attempted add a message to a user, but that user didn't create that message.",
+            str(ObjectMismatch()),
+        )
+
+    def test_LogicError(self):
+        self.assertEqual(
+            "Raised because internal logic has failed. Please create an issue in the github.",
+            str(LogicError()),
+        )
+
+    def test_MessageAssignment(self):
+        err = BaseASHException("A message")
+        self.assertEqual("A message", str(err))
