753f9c28497bf2939abd05677adeb7ab584b80c1
skelmis <skelmis.craft@gmail.com>
2020-10-31 18:49:29 +1300

    :rocket: Distribute Beta

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discord_anti-spam-Skelmis",
+    version="0.1",
+    author="Skelmis",
+    author_email="ethan@koldfusion.xyz",
+    description="An easy to use package for anti-spam features in discord.py.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Skelmis/DPY-Anti-Spam",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires=">=3.6",
+)
