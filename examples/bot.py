aa09ed5c5b9790eb2157e1b92478a75f43eee04f
skelmis <skelmis.craft@gmail.com>
2020-10-31 17:10:01 +1300

    :bulb: Comment examples

--- examples/bot.py
+++ examples/bot.py
@@ -67,7 +67,6 @@ async def _eval(ctx, *, code):
 
 def clean_code(content):
     """Automatically removes code blocks from the code."""
-    # remove ```py\n```
     if content.startswith("```") and content.endswith("```"):
         return "\n".join(content.split("\n")[1:])[:-3]
     else:
