597a373b381792c05f00423bf70d454fe2778184
Skelmis <47520067+Skelmis@users.noreply.github.com>
2020-11-04 14:44:04 +1300

    Updated setup.py
    Just testing pypi atm

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Discord Anti-Spam",
-    version="0.1.1",
+    version="0.2",
     author="Skelmis",
     author_email="ethan@koldfusion.xyz",
     description="An easy to use package for anti-spam features in discord.py.",
