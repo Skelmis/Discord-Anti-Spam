72087406a9502ffb5a91a89a5a8f698c63e148ad
skelmis <skelmis.craft@gmail.com>
2020-12-22 16:20:41 +1300

    :bookmark: V0.4.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="Discord Anti-Spam",
-    version="0.3.2",
+    version="0.4.0",
     author="Skelmis",
     author_email="ethan@koldfusion.xyz",
     description="An easy to use package for anti-spam features in discord.py.",
