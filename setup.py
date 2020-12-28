551d36fdb61205ce63d32a8bb1a199e0aa8965f9
skelmis <skelmis.craft@gmail.com>
2020-12-29 02:36:43 +1300

    :bug: Actually resolve tests

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     test_suite="setup.my_test_suite",
 )
