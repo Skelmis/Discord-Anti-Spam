447c9d8e21dad131424e8a15efbafe86e0637cb4
Skelmis <skelmis.craft@gmail.com>
2021-08-07 18:40:54 +1200

    Work on docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,16 +10,19 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+from sphinx.ext.autodoc import between
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Discord-Anti-Spam'
-copyright = '2021, Skelmis'
-author = 'Skelmis'
+project = "Discord-Anti-Spam"
+copyright = "2021, Skelmis"
+author = "Skelmis"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,10 +31,13 @@ author = 'Skelmis'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.plugins.autodoc",
+    "sphinx.plugins.napoleon",
 ]
 
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,9 +50,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
