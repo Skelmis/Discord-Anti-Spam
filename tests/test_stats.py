717477c95337934f10577c1142349eea74c4960d
skelmis <skelmis.craft@gmail.com>
2021-08-27 08:26:53 +1200

    Begin stats testing

---
+++ tests/test_stats.py
@@ -0,0 +1,8 @@
+from discord.ext.antispam.plugins import Stats  # noqa
+
+from .fixtures import create_bot, create_handler
+
+
+class TestStats:
+    def test_init(self, create_handler):
+        Stats(create_handler)
