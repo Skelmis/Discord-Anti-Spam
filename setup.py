dad42006038d135720aa9d25ef900fe9a7890d9b
skelmis <skelmis.craft@gmail.com>
2021-10-15 03:04:39 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
     ],
     python_requires=">=3.8",
 )
