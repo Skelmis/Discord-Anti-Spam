01043de1aa4694ea610e9b3a1100bd990a2a90d8
skelmis <skelmis.craft@gmail.com>
2021-10-16 17:08:40 +1300

    Up version

--- antispam/__init__.py
+++ antispam/__init__.py
@@ -20,7 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from antispam.anti_spam_handler import AntiSpamHandler
 from antispam.exceptions import *
@@ -35,5 +35,5 @@ from collections import namedtuple
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 version_info = VersionInfo(
-    major=1, minor=0, micro=0, releaselevel="production", serial=0
+    major=1, minor=0, micro=1, releaselevel="production", serial=0
 )
