9747ead333d78a66b2e723bf32626e0063e5316e
skelmis <skelmis.craft@gmail.com>
2021-10-16 16:55:45 +1300

    Add license declaration to all files

--- antispam/plugins/anti_spam_tracker.py
+++ antispam/plugins/anti_spam_tracker.py
@@ -1,8 +1,7 @@
 """
-LICENSE
 The MIT License (MIT)
 
-Copyright (c) 2020-2021 Skelmis
+Copyright (c) 2020-Current Skelmis
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
@@ -20,7 +19,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
-LICENSE
 """
 import datetime
 import logging
