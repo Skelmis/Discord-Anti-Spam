b72227dc7657843dea0c44feb1d249cde1c1072c
skelmis <ethan@koldfusion.xyz>
2021-11-11 14:43:54 +1300

    Fix #76

--- antispam/__init__.py
+++ antispam/__init__.py
@@ -35,3 +35,8 @@ from collections import namedtuple
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 version_info = VersionInfo(major=1, minor=1, micro=1, releaselevel="final", serial=0)
+
+import sys
+
+if sys.version_info[1] < 8:
+    raise RuntimeError("This package requires python 3.8 or higher.")
