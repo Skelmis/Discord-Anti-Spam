4ffbd002b8a15cbd930467cc724730daf1036a62
sigmanificient <sigmanificient@gmail.com>
2021-11-17 19:18:27 +0100

    :recycle: example refactor

--- examples/basic_pincer_example.py
+++ examples/basic_pincer_example.py
@@ -1,5 +1,3 @@
-import logging
-
 from pincer import Client, Intents
 from pincer.objects import UserMessage
 
@@ -7,32 +5,25 @@ from antispam import AntiSpamHandler
 from antispam.enums import Library
 from examples.jsonLoader import read_json
 
-file = read_json("token")
-# logging.basicConfig(
-#     format="%(levelname)s | %(asctime)s | %(module)s | %(message)s",
-#     datefmt="%d/%m/%Y %I:%M:%S %p",
-#     level=logging.DEBUG,
-# )
-
 
 class Bot(Client):
+
+    def __init__(self):
+        super(Bot, self).__init__(
+            token=read_json("token").get("token"),
+            intents=Intents.all()
+        )
+
+        self.antispam = AntiSpamHandler(self, library=Library.PINCER)
+
     @Client.event
     async def on_ready(self):
-        print(
-            f"Started client on {self.bot}\n"
-            "Registered commands: " + ", ".join(self.chat_commands)
-        )
+        print("Logged in as", self.bot)
 
     @Client.event
     async def on_message(self, message: UserMessage):
-        print("on_messages")
         await self.antispam.propagate(message)  # noqa
 
 
-intents = Intents.all()
-# intents >>= 8
-
-b = Bot(file["token"], intents=intents)
-b.antispam = AntiSpamHandler(b, library=Library.PINCER)
-
-b.run()
+if __name__ == '__main__':
+    Bot().run()
